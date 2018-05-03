1d7a375bdc11cfb387082294876b93bb88b41725
agobeaux <alexandre.gobeaux@student.uclouvain.be>
2018-05-04 00:19:38 +0200

    Main Menu Activity (layout only) + pictures used in it

---
+++ MiniPoll/app/src/main/java/be/groupe7lsinf1225/minipoll/activity/MainMenuActivity.java
@@ -0,0 +1,17 @@
+package be.groupe7lsinf1225.minipoll.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import be.groupe7lsinf1225.minipoll.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
