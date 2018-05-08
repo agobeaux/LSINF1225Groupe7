1b51f14080eb3b51b88de024d567570fffdcab0e
gildasmulders <gildas.mulders@student.uclouvain.be>
2018-05-08 14:49:28 +0200

    QuizAdapterStart

---
+++ MiniPoll/app/src/main/java/be/groupe7lsinf1225/minipoll/activity/adapter/QuizAdapter.java
@@ -0,0 +1,65 @@
+package be.groupe7lsinf1225.minipoll.activity.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import be.groupe7lsinf1225.minipoll.R;
+import be.groupe7lsinf1225.minipoll.object.Quiz;
+
+public class QuizAdapter extends BaseAdapter {
+
+    /**
+     * Permet d'instancier un fichier xml de layout dans une vue.
+     */
+    private final LayoutInflater mInflater;
+
+    /**
+     * Liste des éléments de collection à mettre dans la liste.
+     */
+    private ArrayList<Quiz> quizzes;
+
+    /**
+     * Constructeur.
+     *
+     * @param context        Contexte de l'application.
+     * @param quizzes Liste des éléments de collection à placer dans la liste.
+     */
+    public MyListViewAdapter(Context context, ArrayList<Quiz> quizzes) {
+        mInflater = LayoutInflater.from(context);
+        this.quizzes = quizzes;
+    }
+
+    @Override
+    public int getCount() {
+        return quizzes.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return quizzes.get(i);;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i; //idealement aller chercher id de quiz dans sql
+    }
+
+    @Override
+    public View getView(int i, View view, View convertView, ViewGroup viewGroup) {
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.collected_friend_row, viewGroup, false);
+        }
+        TextView topic = convertView.findViewById(R.id.show_row_topic);
+        TextView limitdate = convertView.findViewById(R.id.show_row_limitdate);
+        TextView createdby = convertView.findViewById(R.id.show_row_createdby);
+
+        return null;
+    }
+}
