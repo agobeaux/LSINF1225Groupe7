9daa044e0d574ee52b21139f8b37dbb434c67acb
agobeaux <alexandre.gobeaux@student.uclouvain.be>
2018-05-08 15:41:14 +0200

    partie de Quiz

---
+++ MiniPoll/app/src/main/java/be/groupe7lsinf1225/minipoll/object/QuizActivity.java
@@ -0,0 +1,15 @@
+package be.groupe7lsinf1225.minipoll.object;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import be.groupe7lsinf1225.minipoll.R;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
